dc2e63a557c7b54574cbd04324c6e836d7c0052c
gajehv <gajehv@LIN83003103.corp.capgemini.com>
2023-06-22 17:46:58 +0530

    hii

---
+++ gitpractice1/src/gitpractice1/Add1.java
@@ -0,0 +1,16 @@
+package gitpractice1;
+
+import java.util.Scanner;
+
+public class Add1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("addition of a & b is "+c);
+		System.out.println("subtraction of a & b is "+(a-b));
+       
+	}
+
+}
