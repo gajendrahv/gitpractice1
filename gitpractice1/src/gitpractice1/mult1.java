b28b58a687ce196bf28413982e7a9b32b427bdbf
gajehv <gajehv@LIN83003103.corp.capgemini.com>
2023-06-22 18:13:56 +0530

    hello2

---
+++ gitpractice1/src/gitpractice1/mult1.java
@@ -0,0 +1,16 @@
+package gitpractice1;
+
+import java.util.Scanner;
+
+public class mult1 {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a*b;
+		System.out.println("multiplication of a & b is "+c);
+		
+	} 
+
+}
