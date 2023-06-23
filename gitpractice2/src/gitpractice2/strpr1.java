fcba5e219488c5840a62807cebe6d35783ff37eb
gajehv <gajehv@LIN83003103.corp.capgemini.com>
2023-06-23 11:46:35 +0530

    swami

---
+++ gitpractice2/src/gitpractice2/strpr1.java
@@ -0,0 +1,12 @@
+package gitpractice2;
+
+public class strpr1 {
+	
+	public static void main(String[] args) {
+		String str = "hello world",
+				abc = str.toUpperCase();
+		System.out.println(abc);
+	}
+   
+
+}
