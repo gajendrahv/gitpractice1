d96a39b36bbc60e9d25a8ccb262dec4536d6dd72
gajehv <gajehv@LIN83003103.corp.capgemini.com>
2023-06-23 12:43:10 +0530

    gaja3

--- gitpractice3/src/gitpractice3/exp1.java
+++ gitpractice3/src/gitpractice3/exp1.java
@@ -1,5 +1,10 @@
 package gitpractice3;
 
 public class exp1 {
-
+	
+	public static void main(String[] args) {
+		String text1 = "Hello ";
+		String text2 = "World";
+		System.out.println(text1.concat(text2));
+	}
 }
